c8cf72d4ae6e31502add8fe63a5ffed2c454e14e
Unknown <adh416@nyu.edu>
2017-11-14 00:14:59 -0500

    Class Build
    Added sound and fixed bugs
    Removed platform from The Clock
    Added like 40% of the sound

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// is called before Start
+	void Awake () {
+        // Adds an audio source for each sound in our sounds array
+		foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+        }
+	}
+
+    void Start()
+    {
+        Play("Wind");
+    }
+
+    public void Play(string name)
+    {
+        //Find the sound in the sounds array where sounds.name is equal to the name
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not found");
+            return;
+        }
+        s.source.Play();
+    }
+}
+
+   
